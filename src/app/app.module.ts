5c8a74a29a3eca8c511951b6f093de91ce24ebb6
unknown <s.nanthagopal1998@gmail.com>
2022-08-23 18:06:42 +0530

    [Nantha] - Component Created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './component/register/register.component';
+import { LoginComponent } from './component/login/login.component';
+import { ForgotComponent } from './component/forgot/forgot.component';
+import { ResetComponent } from './component/reset/reset.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    ForgotComponent,
+    ResetComponent
   ],
   imports: [
     BrowserModule,
